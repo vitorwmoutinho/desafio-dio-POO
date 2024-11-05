d67c5a2880e6ece7623aa24ddf71fc7e0c6fc996
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-04 21:27:11 -0300

    Implementação e atributos

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição de Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descrição de Curso Javascript");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição da mentoria");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+
+    }
+
 }
